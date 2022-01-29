3c1bbfcbe802065812ad5b760f058a82df08d5be
Ingo Fischer <github@fischer-ka.de>
2022-01-29 15:27:35 +0100

    update testAdapter.js

--- test/testFunctions.js
+++ test/testFunctions.js
@@ -49,7 +49,7 @@ describe('Test DoorIO', function () {
         this.timeout(600000); // because of first install from npm
 
         setup.setupController(() => {
-            const config = setup.getAdapterConfig();
+            const config = await setup.getAdapterConfig();
             // enable adapter
             config.common.enabled  = true;
             config.common.loglevel = 'debug';
@@ -61,7 +61,7 @@ describe('Test DoorIO', function () {
                 }
             ];
 
-            setup.setAdapterConfig(config.common, config.native);
+            await setup.setAdapterConfig(config.common, config.native);
 
             setup.startController(
                 true,
